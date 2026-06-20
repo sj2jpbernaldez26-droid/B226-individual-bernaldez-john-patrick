b5d4097be9fdf59277ce1a3c754cf9abf5a6d121
John <sj2.jpbernaldez26@joysistvi.edu.ph>
2026-06-20 16:24:35 +0800

    initial commit

---
+++ build/classes/SampleCode2/java.rs
@@ -0,0 +1,2 @@
+day2.SampleCode2
+day2.discussion.TestMain
---
+++ src/day2/discussion/SampleCode.java
@@ -0,0 +1,17 @@
+
+package day3.discussion; // Package
+
+public class SampleCode { // class
+    
+    public static void main(String[] args) { // Main Method
+        System.out.println("Hello World"); // print statement
+    }
+}
+
+// Main Method(psvm) -> starting point of programs execution
+// Print Statement (sout) -> it tells the computer to print something
+/*
+    2 Types of Comments
+    1. Single Line comment ( // )
+    2. Multi line Comment ( /* )
+*/
