f36245f9769ec14137eebfa43d403de5523a68b5
John <sj2.jpbernaldez26@joysistvi.edu.ph>
2026-06-23 17:15:27 +0800

    Adding Activity1 and Activity2

--- src/day5/discussion/Activity2_Bernaldez.java
+++ src/day5/discussion/Activity2_Bernaldez.java
@@ -5,10 +5,31 @@ public class Activity2_Bernaldez {
     
     public static void main(String[] args) {
         
-        double income =50000;
-        double foodAllowance =15000;
+        double income = 50000;
+        double foodAllowance = 15000;
+        double transportation = 2500;
+        double rent = 4500;
+        double utilityBill = 3500;
+        
+        double foodPercent = (foodAllowance / income) * 100;
+        double transportationPercent = (transportation / income) * 100;
+        double rentPercent = (rent / income) * 100;
+        double utilityPercent = (utilityBill / income) * 100;
+        
+        double totalExpenses = foodAllowance + transportation + rent + utilityBill;
+        double remainingBalance = income - totalExpenses; 
+       
+        // %.1f -> prints a decimal number with 1 digit afte the decimal point
+        // %% -> prints the % symbol
+        // \n -> goes to the next line
+        System.out.printf("Food Allowance: %.1f %%\n", foodPercent);
+        System.out.printf("Trasportation: %.1f %%\n", transportationPercent);
+        System.out.printf("Rent: %.1f %%\n", rentPercent);
+        System.out.printf("Utility Bill: %.1f %%\n", utilityPercent);
         
         
-        
+        System.out.println("Remaining Balance: " + remainingBalance);
+            
     }
+    
 }
