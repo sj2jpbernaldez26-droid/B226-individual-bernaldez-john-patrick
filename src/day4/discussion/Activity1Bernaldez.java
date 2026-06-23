a2d3e8e58a3e0fd35484922c3989202aa330609b
John <sj2.jpbernaldez26@joysistvi.edu.ph>
2026-06-23 11:46:55 +0800

    Complete Day 4 Java data types and variables exercises

---
+++ src/day4/discussion/Activity1Bernaldez.java
@@ -0,0 +1,27 @@
+package day4.discussion;
+
+public class Activity1Bernaldez {
+    public static void main(String[] args) {
+
+
+        boolean isMale = true;
+        byte age = 24;
+        short birthYear = 2001;
+        int zipCode = 1478;
+        long phoneNumber = 639167048073l;
+        float height = 5.8f;
+        double temp = 36.55;
+        char bloodType = 'O';
+        String name = "John Patrick Bernaldez";
+
+        System.out.println("Is He a Male: " + isMale);
+        System.out.println("Age: " + age);
+        System.out.println("Year of Birth: " + birthYear);
+        System.out.println("Zip Code: " + zipCode);
+        System.out.println("Phone Number: " + phoneNumber);
+        System.out.println("Height: " + height);
+        System.out.println("Temperature: " + temp);
+        System.out.println("Blood Type: " + bloodType);
+        System.out.println("Name: " + name);
+    }
+}
---
+++ src/day4/discussion/DataType.java
@@ -0,0 +1,17 @@
+package day4.discussion;
+
+
+public class DataType {
+    public static void main(String[] args) {
+        boolean isActive = true;
+        char letter = 'A';
+        byte num = 50;
+        short num2 = 200;
+        int num3 = 10_000_000;
+        long num4 = 100_000_000_00l; // suffix l/L
+        float temp = 35.50f; // suffix f/F
+        double average = 98.01230429;
+        String word = "Java";
+        
+    }
+}
